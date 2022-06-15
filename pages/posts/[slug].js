93597da2d876de4d461d260393f8e176a7fba283
austinkelsay <austinkelsay@yahoo.com>
2022-06-14 19:48:53 -0500

    Added dummy comments on slug page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,29 +1,51 @@
 import { useRouter } from 'next/router'
 import {useState, useEffect} from "react"
+import styles from "./styles.module.css"
 import axios from "axios"
 
 const Post = () => {
   const router = useRouter()
   const { slug } = router.query
   const [post, setPost] = useState([])
+  const [comments, setComments] = useState([])
   console.log(slug)
 
   useEffect(() => {
     if (typeof(slug) !== 'undefined') {
         axios.get(`https://jsonplaceholder.typicode.com/posts/${slug}`)
         .then(res => {
-            console.log(res.data)
             setPost(res.data)
         })
         .catch(err => console.log(err))
     }
   }, [])
 
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${slug}/comments`)
+        .then(res => {
+            console.log(res.data)
+            setComments(res.data)
+        })
+        .catch(err => console.log(err))
+  }, [post])
+
 
   return (
     <div>
-        <h1>{post.title}</h1>
-        <p>{post.body}</p>
+        <div className={styles.post}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+        {
+            comments.map(comment => {
+                return (
+                    <div className={styles.commentContainer}>
+                        <h4>{comment.name}</h4>
+                        <p>{comment.body}</p>
+                    </div>
+                )
+            })
+        }
     </div>
   )
 }
