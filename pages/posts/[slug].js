0bfcde76bfce1903d87a63ec0fd70a82aa21cc5e
austinkelsay <austinkelsay@yahoo.com>
2022-06-15 14:20:11 -0500

    Starting comment dataContainer example using primereact

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,12 @@
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
 import { useRouter } from 'next/router'
 import {useState, useEffect} from "react"
+import { DataScroller } from 'primereact/datascroller';
+import { Button } from 'primereact/button';
+import Sidebar from '../../components/sidebar';
 import styles from "./styles.module.css"
 import axios from "axios"
 
@@ -29,23 +36,29 @@ const Post = () => {
         .catch(err => console.log(err))
   }, [post])
 
+  const itemTemplate = (data) => {
+    return (
+        <div className={styles.comment}>
+            <span>{data.email}</span>
+            <h4>{data.name}</h4>
+            <p>{data.body}</p>
+            <div className={styles.upvoteContainer}>
+                <Button icon="pi pi-thumbs-up" disabled={'OUTOFSTOCK'}></Button>
+            </div>
+        </div>
+    );
+}
 
   return (
     <div>
+        <Sidebar />
         <div className={styles.post}>
             <h1>{post.title}</h1>
             <p>{post.body}</p>
         </div>
-        {
-            comments.map(comment => {
-                return (
-                    <div className={styles.commentContainer}>
-                        <h4>{comment.name}</h4>
-                        <p>{comment.body}</p>
-                    </div>
-                )
-            })
-        }
+        <div className={styles.DataScroller}>
+            <DataScroller value={comments} itemTemplate={itemTemplate} rows={comments?.length} scrollHeight="auto" header="Answers" />
+        </div>
     </div>
   )
 }
